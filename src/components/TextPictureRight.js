a0d530f8ac35636f17c06ade43efb01600c42a21
Kevin U <kevin_udema@hotmail.com>
2025-12-08 17:05:35 +0100

    changed layout slightly to better fit mobile
    Flexbox center content and slightly different alignment used for mobile

--- src/components/TextPictureRight.js
+++ src/components/TextPictureRight.js
@@ -1,27 +1,25 @@
 function TextPictureRight({pictureRightImg, textTitle, mainText}) {
   return (
     <section className="textWithPictureRight backgroundblack">
-      <div className="container">
-        <div
-          data-inviewport="scale-in"
-          className="d-flex align-items-center fullview example-style"
-        >
-          <div className="col-sm-6 text-white">
-          <h1 className="display-1">{textTitle}</h1>
-            <h1 className="p-2">Maybe small title before text</h1>
-            <p className="p-2">
-              {mainText}
-            </p>
-          </div>
-          <div className="col-sm-5 offset-1 text-white">
-            <img
-              className="img-fluid bordershadows"
-              src={pictureRightImg}
-              alt="Stagebreaker" 
-            />
+      <div className="container vh-100 d-flex align-items-center">
+        <div className="row">
+            <div className="col-sm-6 col-md-6 col-xs-12 text-white">
+              <h1 className="display-1">{textTitle}</h1>
+              <h1 className="p-2">Maybe small title before text</h1>
+              <p className="p-2">
+                {mainText}
+              </p>
+            </div>
+            <div className="col-sm-6 col-md-6 text-white">
+              <img
+                className="img-fluid bordershadows"
+                src={pictureRightImg}
+                alt="Stagebreaker" 
+              />
+            </div>
           </div>
         </div>
-      </div>
+        <div className="clearfix"></div>
     </section>
   );
 }
